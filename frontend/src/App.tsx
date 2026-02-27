a1c5c81e7cab0dfcb85f0c5914a686394d3eab3b
ashleybarreiro11 <ashley.barreiro@u.icesi.edu.com>
2026-02-26 20:20:23 -0500

    fourth commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await fetch("http://localhost:3000/posts");
+      const res = await fetch("https://lab2-express.vercel.app/");
       const data = await res.json();
       console.log(data);
       setPosts(data);
@@ -26,7 +26,7 @@ function App() {
   }, []);
 
   const post = async (title: string, img_url: string, description: string) => {
-    const res = await fetch("http://localhost:3000/post", {
+    const res = await fetch("https://lab2-express.vercel.app/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -46,7 +46,7 @@ function App() {
   };
 
   const deletePost = async (id: string) => {
-    const res = await fetch(`http://localhost:3000/post/` + id, {
+    const res = await fetch(`https://lab2-express.vercel.app/` + id, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
